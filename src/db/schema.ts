749cf6ea35220f2acb15b185b513eadbe839274a
Adarsh Dubey <dubeyadarshmain@gmail.com>
2025-10-31 22:28:54 +0530

    feat: implement drizzle orm
    and also include scripts to work with drizzle-kit

---
+++ src/db/schema.ts
@@ -0,0 +1,20 @@
+import {
+  boolean,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const issues = pgTable("issues", {
+  id: serial("id").primaryKey(),
+  title: varchar("title", { length: 255 }).notNull(),
+  slug: varchar("slug", { length: 255 }).notNull().unique(),
+  description: text("description"),
+  content: text("content"),
+  isPublished: boolean("is_published").default(false).notNull(),
+  publishedAt: timestamp("published_at"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
